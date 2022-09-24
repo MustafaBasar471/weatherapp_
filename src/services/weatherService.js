99776c544be41547adfd928d2c7b47f734b58f79
MustafaBasar471 <mustafabasar471@gmail.com>
2022-09-24 22:13:32 +0300

    ç

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -5,7 +5,6 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
-  console.log(process.env);
   url.search = new URLSearchParams({
     ...searchParams,
     appid: API_KEY,
